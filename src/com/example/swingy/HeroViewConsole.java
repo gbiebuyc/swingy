72f652bf38dbdcf2e9b67f27cc7d27d2930a55dd
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-08-24 14:28:12 +0200

    update

--- src/com/example/swingy/HeroViewConsole.java
+++ src/com/example/swingy/HeroViewConsole.java
@@ -1,6 +1,7 @@
 package com.example.swingy;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HeroViewConsole implements HeroView {
 	public HeroModel selectHero(ArrayList<HeroModel> heroes) {
@@ -11,6 +12,28 @@ public class HeroViewConsole implements HeroView {
 			System.out.printf("%d - %s, %s, LVL %d, %d XP, %d Attack, %d Defense, %d HP\n",
 					i+1, h.name, h.heroClass, h.level, h.xp, h.getAttackPoints(),
 					h.getDefensePoints(), h.getHitPoints());
+		}
+		System.out.println("q - Quit");
+		@SuppressWarnings("resource")
+		Scanner scan = new Scanner(System.in);
+		while (true) {
+			System.out.print("Your answer: ");
+			String[] lineSplited = scan.nextLine().split("\\s+");
+			if (lineSplited.length != 1) {
+				System.out.println("Bad input");
+				continue;
+			}
+			if (lineSplited[0].equalsIgnoreCase("q")) {
+				System.out.println("Bye");
+				System.exit(0);
+			}
+			try {
+				int n = Integer.parseInt(lineSplited[0]);
+				break;
+			} catch (Exception e) {
+				System.out.println("Bad input");
+				continue;
+			}
 		}
 		return null;
 	}
